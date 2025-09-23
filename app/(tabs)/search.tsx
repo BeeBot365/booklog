31ba7e7060952d8579c188702e9048ae11c40aa1
Nasstrom33 <nasstrom.alex@gmail.com>
2025-09-23 15:53:36 +0200

    Add useState to textinput

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,6 +1,7 @@
 import BookCard from "@/components/books/book-card";
 import { ThemedText } from "@/components/themed-text";
 import { mockedbooks } from "@/src/feature/books/data/book.types";
+import { useState } from "react";
 import {
   View,
   Text,
@@ -11,10 +12,17 @@ import {
 } from "react-native";
 
 export default function Search() {
+  const [value, setValue] = useState<string>("");
   const books = mockedbooks;
   return (
     <View style={styles.container}>
-      <TextInput placeholder="Search book by title.."></TextInput>
+      <TextInput
+        style={styles.input}
+        placeholder="Search book by title.."
+        placeholderTextColor="#888"
+        onChangeText={setValue}
+        value={value}
+      ></TextInput>
       <FlatList
         data={books}
         horizontal={false}
@@ -35,5 +43,13 @@ const styles = StyleSheet.create({
   },
   listContent: {
     paddingBottom: 24,
+  },
+  input: {
+    fontSize: 16,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 8,
+    borderRadius: 8,
+    marginBottom: 12,
   },
 });
