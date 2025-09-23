149e083e9e35b15df1ce119f795afd8880560d7e
Nasstrom33 <nasstrom.alex@gmail.com>
2025-09-23 14:59:05 +0200

    Add Button component

---
+++ components/ui/button.tsx
@@ -0,0 +1,50 @@
+import { Pressable, Text } from "react-native";
+
+interface Props {
+  variant: "red" | "blue" | "green" | "gray";
+  padding: number;
+  fontSize: number;
+  value: string;
+  borderRadius?: number;
+  onPress: () => void;
+}
+export default function Button(props: Props) {
+  let backgroundColor: string;
+  switch (props.variant) {
+    case "red":
+      backgroundColor = "#cb2828ff";
+      break;
+    case "blue":
+      backgroundColor = "#185abc";
+      break;
+    case "green":
+      backgroundColor = "#10B981";
+      break;
+    case "gray":
+      backgroundColor = "#444444";
+      break;
+  }
+
+  return (
+    <Pressable
+      onPress={props.onPress}
+      style={{
+        backgroundColor: backgroundColor,
+        padding: props.padding,
+        borderRadius: props.borderRadius ?? 0,
+        alignItems: "center",
+        justifyContent: "center",
+        marginVertical: 2,
+      }}
+    >
+      <Text
+        style={{
+          fontSize: props.fontSize,
+          color: "#FFFFFF",
+        }}
+      >
+        {props.value}
+      </Text>
+    </Pressable>
+  );
+}
