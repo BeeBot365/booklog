41cd46ae1636f12291c8e6927b214e8da9d201a5
Nasstrom33 <nasstrom.alex@gmail.com>
2025-09-25 13:38:59 +0200

    setup stack screens for search

--- app/(tabs)/Search/_layout.tsx
+++ app/(tabs)/Search/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function SearchLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="search" options={{ headerShown: false }} />
+      <Stack.Screen name="book/[id]" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
