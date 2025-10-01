5601156ae266490e846a227c0c5c3cb032c909af
Nasstrom33 <nasstrom.alex@gmail.com>
2025-10-01 15:22:03 +0200

    setup profile tab/screens

---
+++ app/(tabs)/profile/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+
+export default function SearchLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ title: "Settings", headerShown: true }}
+      />
+      <Stack.Screen name="[id]" options={(props) => ({ headerShown: true })} />
+    </Stack>
+  );
+}
