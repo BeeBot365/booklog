2110bc45d13a1d1a1b8078e6f19b8bfcb0570d1c
Nasstrom33 <nasstrom.alex@gmail.com>
2025-10-01 12:55:13 +0200

    removed all presetups from expo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,6 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { SQLiteProvider } from "expo-sqlite";
-
-import { useColorScheme } from "@/hooks/use-color-scheme";
 import { initDatabase } from "@/src/db/db";
 import BooksProvider from "@/providers/books-provider";
 import Toast from "react-native-toast-message";
@@ -17,20 +10,14 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <SQLiteProvider databaseName="bookLog.db" onInit={initDatabase}>
       <BooksProvider>
-        <ThemeProvider
-          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-        >
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          </Stack>
-          <Toast />
-          <StatusBar style="auto" />
-        </ThemeProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+        <Toast />
+        <StatusBar style="auto" />
       </BooksProvider>
     </SQLiteProvider>
   );
