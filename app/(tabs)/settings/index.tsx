efce7f713e01baa9e72cfa4fa60ce6e1c70d97a0
Nasstrom33 <nasstrom.alex@gmail.com>
2025-10-03 18:09:01 +0200

    fixed bug ib settings screen

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -1,132 +1,3 @@
-// import React, { use, useEffect, useState } from "react";
-// import { View, Text, Switch, StyleSheet } from "react-native";
-
-// import DateTimePicker, {
-//   DateTimePickerEvent,
-// } from "@react-native-community/datetimepicker";
-// import Button from "@/components/ui/button";
-// import {
-//   loadReminder,
-//   resetReminder,
-//   saveReminder,
-// } from "@/src/securestore/reminder";
-
-// export default function SettingsScreen() {
-//   const [reminderEnabled, setReminderEnabled] = useState<boolean>(false);
-//   const [reminderTime, setReminderTime] = useState<Date>(new Date());
-//   const [showPicker, setShowPicker] = useState<boolean>(false);
-
-//   const toggleReminder = () => {
-//     setReminderEnabled(!reminderEnabled);
-//   };
-
-//   const onChangeTime = (
-//     event: DateTimePickerEvent,
-//     selectedDate: Date | undefined
-//   ) => {
-//     setShowPicker(false);
-//     if (selectedDate) {
-//       setReminderTime(selectedDate);
-//     }
-//   };
-
-//   useEffect(() => {
-//     async function loadSecureStore() {
-//       const value = await loadReminder();
-//       if (value) {
-//         setReminderEnabled(true);
-//         setReminderTime(new Date(value as string));
-//         console.log(
-//           "UseEffect körs och sätter värden från SecureStore och value är: ",
-//           value
-//         );
-//       }
-//     }
-//     loadSecureStore();
-//   }, []);
-
-//   useEffect(() => {
-//     async function saveOrResetReminder() {
-//       if (!reminderEnabled) {
-//         await resetReminder();
-//         console.log("Reminder är avstängd och värdet är raderat i SecureStore");
-//         return;
-//       }
-//       setReminderTime(new Date());
-//       await saveReminder(reminderTime);
-//     }
-//     saveOrResetReminder();
-//   }, [reminderEnabled]);
-
-//   useEffect(() => {
-//     async function saveTimeChange() {
-//       if (reminderEnabled) {
-//         await saveReminder(reminderTime);
-//         console.log("tiden har ändrats till: ", reminderTime);
-//       }
-//     }
-//     saveTimeChange();
-//   }, [reminderTime]);
-
-//   return (
-//     <View style={styles.container}>
-//       <View style={styles.view}>
-//         <Text style={{ fontSize: 18 }}>Slå på påminnelse</Text>
-//         <Switch value={reminderEnabled} onValueChange={toggleReminder} />
-//       </View>
-
-//       {reminderEnabled && (
-//         <View>
-//           <Text style={styles.text}>
-//             Påminnelsetid:{" "}
-//             {reminderTime
-//               ? reminderTime.toLocaleTimeString("sv-SE")
-//               : "Ingen tid vald"}
-//           </Text>
-
-//           <Button
-//             value="Ändra tid"
-//             onPress={() => setShowPicker(true)}
-//             variant={"blue"}
-//             padding={10}
-//             fontSize={16}
-//             borderRadius={10}
-//           />
-
-//           {showPicker && (
-//             <DateTimePicker
-//               value={reminderTime}
-//               mode="time"
-//               is24Hour={true}
-//               display="default"
-//               onChange={onChangeTime}
-//             />
-//           )}
-//         </View>
-//       )}
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     padding: 20,
-//   },
-
-//   view: {
-//     flexDirection: "row",
-//     justifyContent: "space-between",
-//     alignItems: "center",
-//     marginBottom: 20,
-//   },
-
-//   text: {
-//     fontSize: 16,
-//     marginBottom: 10,
-//   },
-// });
-
 import { useEffect, useState } from "react";
 import { View, Text, Switch, StyleSheet, Platform, Alert } from "react-native";
 
@@ -202,8 +73,8 @@ export default function SettingsScreen() {
     async function loadSecureStore() {
       const value = await loadReminder();
       if (value) {
-        setReminderEnabled(true);
         setReminderTime(new Date(value as string));
+        setReminderEnabled(true);
       }
     }
     loadSecureStore();
@@ -214,11 +85,8 @@ export default function SettingsScreen() {
       if (!reminderEnabled) {
         await resetReminder();
         await Notifications.cancelAllScheduledNotificationsAsync();
-        setReminderTime(new Date());
         return;
       }
-      await saveReminder(reminderTime);
-      await scheduleDailyAt(reminderTime);
     }
     saveOrResetReminder();
   }, [reminderEnabled]);
@@ -231,7 +99,7 @@ export default function SettingsScreen() {
       }
     }
     saveTimeChange();
-  }, [reminderTime]);
+  }, [reminderTime, reminderEnabled]);
 
   return (
     <View style={styles.container}>
